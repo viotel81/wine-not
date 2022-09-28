0d252f586152af8bea7c1fe2a691969dd1b8c613
Simona Sriube <simona.martinkute3@gmail.com>
2022-09-28 11:04:24 +0300

    update 28-09

--- client/src/components/wines/Wines.jsx
+++ client/src/components/wines/Wines.jsx
@@ -1,14 +1,24 @@
 import Wine from '../wine/Wine';
 import './wines.css';
+import Carousel from 'react-bootstrap/Carousel';
+
+
 
 function Wines({ wines }) {
+
     return (
         <>
             <div className="winesmain">
                 <h3><b>Take Your Time and Enjoy the Tasty of the <br />World of Wines</b></h3>
-                {wines.map((w) => (
-                     <Wine key={w._id} wine={w}/>
-                ))}
+              
+                <Carousel>
+                    {wines.map((w) => (
+                        <Carousel.Item>
+                            <Wine key={w._id} wine={w} />
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
+               
             </div>
         </>
     )
